053389abd1f30944e3a3219fb4251fa72aa3a0c6
Ahmed Ammar <aammar@genesi-usa.com>
2013-05-13 14:37:03 +0200

    nullptr replacement added

--- widget/gtk2/nsDbusmenu.cpp
+++ widget/gtk2/nsDbusmenu.cpp
@@ -9,6 +9,19 @@
 #include "prlink.h"
 #include "mozilla/Util.h"
 
+const                        // this is a const object...
+class {
+public:
+  template<class T>          // convertible to any type
+    operator T*() const      // of null non-member
+    { return 0; }            // pointer...
+  template<class C, class T> // or any type of null
+    operator T C::*() const  // member pointer...
+    { return 0; }
+private:
+  void operator&() const;    // whose address can't be taken
+} nullptr = {};              // and whose name is nullptr
+
 #define FUNC(name, type, params) \
 nsDbusmenuFunctions::_##name##_fn nsDbusmenuFunctions::s_##name;
 DBUSMENU_GLIB_FUNCTIONS
